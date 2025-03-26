0322c292d440d3c8daf2b3fbe26d66fe53274edb
darshan panchal <darshan@MacBookPro.hub>
2025-03-26 17:42:35 +1300

    updated use client

--- app/api/deleteComment/route.js
+++ app/api/deleteComment/route.js
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 import connectDB from "../../../lib/connectDb";
 import Comment from "../../models/Comment";
+
 export async function DELETE(req) {
   try {
     await connectDB();
